083bc7ae9206f25b231d233c76658639a2c5f042
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2026-03-26 11:36:55 +0100

    Delete unused functions from site.js
    Remove IIFE and utility functions from site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,16 +1,1 @@
-﻿(function () {
-const facilityContentId = "facility-content";
-const btnAllKeysId = "btnAllKeys";
 
-function getFacilityContainer() {
-return document.getElementById(facilityContentId);
-}
-
-function debounce(fn, delay) {
-let t;
-return (...args) => {
-clearTimeout(t);
-t = setTimeout(() => fn(...args), delay);
-};
-}
-})();
