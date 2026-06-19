c5936134a5e0fc53d17d265467f57544b01e50f5
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2026-06-19 14:55:04 +0100

    Add administrator views

---
+++ Views/Administrator/_AssignmentForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KEYREGISTERAUTOMATION.Views.Administrator
+{
+    public class _AssignmentFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
