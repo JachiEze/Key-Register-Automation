f53a9f23f40ff365c6be238fb2a11c88f583a8a2
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2026-06-19 11:46:02 +0100

    Update Assignment.cs

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -1,19 +1,29 @@
-﻿namespace KEYREGISTERAUTOMATION.Models
+using Microsoft.EntityFrameworkCore;
+
+namespace KEYREGISTERAUTOMATION.Models
 {
     public class AssignmentRecord
     {
         public int Id { get; set; }
-        public string RequesterIGG { get; set; }
-        public string RequesterName { get; set; }
-        public string Department { get; set; }
-        public string Division { get; set; }
-        public string CollectorType { get; set; }
+        public string? RequesterIGG { get; set; }
+        public string? RequesterName { get; set; }
+        public string? Department { get; set; }
+        public string? Division { get; set; }
+        public string? CollectorType { get; set; }
         public string? DelegateName { get; set; }
-        public string AssignmentType { get; set; }
+        public string? AssignmentType { get; set; }
         public string Status { get; set; } = "Issued";
-        public string KeyId { get; set; }
+        public string? RoomNumber { get; set; }
+        public string? FloorNumber { get; set; }
+        public string? KeyId { get; set; }
+        public string? TagNumber { get; set; }
+        public int? IndividualKeyId { get; set; }
         public DateTime? DateRequested { get; set; } = DateTime.Now;
         public int? Duration { get; set; }
         public DateTime? DueDate { get; set; }
+        public DateTime AssignedOn { get; set; }
+        public DateTime? ReturnedOn { get; set; }
+        public string? Comment {  get; set; }
+        public bool Reminder3Days { get; set; } = false;
     }
 }
