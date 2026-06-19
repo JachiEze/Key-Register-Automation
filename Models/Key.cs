19dd047c29138e59e4696068861f571307e6e90a
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2026-06-19 11:48:07 +0100

    Update Key.cs

--- Models/Key.cs
+++ Models/Key.cs
@@ -2,17 +2,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace KEYREGISTERAUTOMATION.Models
 {
-    public class AllKeyTags
+    public class Keys
     {
+        public const string DefaultKeyCode = "TF441910";
         public int Id { get; set; }
-        public string KeyId { get; set; }
-        public string KeyCode { get; set; }
-        public string Building { get; set; }
-        public string RoomNumber { get; set; }
-        public string FloorNumber { get; set; }
+        public string? KeyId { get; set; }
+        public string? KeyCode { get; set; } = DefaultKeyCode;
+        public string? Building { get; set; }
+        public string? RoomNumber { get; set; }
+        public string? FloorNumber { get; set; }
         public int TotalNoofKeys { get; set; }
         public int NoofKeysAvaialble { get; set; }
-
+        public ICollection<IndividualKey> IndividualKeys { get; set; } = new List<IndividualKey>();
     }
+
 }
 
