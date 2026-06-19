2167ce116ec3d02e06b73ff7f5f75071e2a0ad8d
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2026-06-19 11:42:37 +0100

    Add Graphs.cs

---
+++ Models/ViewModels/Graphs.cs
@@ -0,0 +1,9 @@
+namespace KEYREGISTERAUTOMATION.Models.ViewModels
+{
+    public class Graphs
+    {
+        public List<string> Floors { get; set; } = new();
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
