b7c158c87e562a5401a9de7c5ff8247c61ddc0c1
Jachimike Divine Ezenwamadu <94161891+JachiEze@users.noreply.github.com>
2026-06-19 11:43:38 +0100

    Add ReturnAssignment.cs

---
+++ Models/ViewModels/ReturnAssignment.cs
@@ -0,0 +1,8 @@
+namespace KEYREGISTERAUTOMATION.Models.ViewModels
+{
+    public class ReturnAssignment
+    {
+        public int Id { get; set; }
+        public string? Comment { get; set; }
+    }
+}
